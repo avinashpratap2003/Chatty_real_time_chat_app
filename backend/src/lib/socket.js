e05b6961af9b8fe46eaba046b7554f05140812fd
avinashpratap2003 <pratapavinash843@gmail.com>
2025-06-24 21:44:30 +0530

    removed link

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://chatty-real-time-chat-4zimxebfx-avinashpratap2003s-projects.vercel.app"],
+    origin: "http://localhost:5173",
   },
 });
 
